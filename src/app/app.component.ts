43919117c101f655f726a6675d3f1a92aff5e802
Ramachandiran <chandiran666@gmail.com>
2020-10-28 14:27:08 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {ToastrService} from 'ngx-toastr'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pdf-converter';
+  serverURL="http://localhost:8082/text-pdf"
+  textForm:FormGroup
+  constructor(private toastr:ToastrService,private formbuilder:FormBuilder,private http:HttpClient){}
+  ngOnInit():void{
+
+    this.textForm=this.formbuilder.group({
+      text:new FormControl()
+    })
+
+  }
+  saveAsPdf()
+  {
+    if(this.textForm.value.text==null  || this.textForm.value.text.trim().length<1)
+    {
+     this.toastr.warning('please provide some content')
+    }
+    else{
+    this.http.post(this.serverURL,this.textForm.value,{responseType:'text' as 'json'}).subscribe(result=>{
+      if(result=='success')
+      {
+        this.toastr.success('PDF conversion done successfully')
+        this.textForm.reset()
+      }
+    },
+    error=>
+    {
+      console.log('oops something went wrong')
+      this.toastr.error('oops something went wrong')
+    })
+  }
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms'
+ 
+import { ToastrModule, } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -10,7 +15,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ToastrModule.forRoot({
+      closeButton:true,
+      progressBar:true,
+      timeOut:4000
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
